f478e91417a0760ea9da83766903e976c9f1606d
sumonjsr <157957786+sumonjsr@users.noreply.github.com>
2026-06-07 10:52:12 +0000

    fix: trailingSlash never with file format

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
-  // আপনার Netlify সাইটের ইউআরএল
   site: 'https://quickfincalc.com',
-  
-  // 'always' থেকে পরিবর্তন করে 'ignore' করা হয়েছে যাতে স্ল্যাশ নিয়ে ঝামেলা না হয়
-  trailingSlash: 'ignore', 
-
+  trailingSlash: 'never',
   output: 'static',
-
-  // সitemap এখন খালি রাখা হয়েছে
   integrations: [sitemap()],
-
-  // Build settings
   build: {
-    format: 'directory', 
+    format: 'file',
   }
 });
